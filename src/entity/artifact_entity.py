142d26d4d7df92f35f9775c6999c68a9485b4d21
JaiPrakash <pjaiprakash2000@gmail.com>
2026-04-20 15:21:33 +0530

    Data Ingestion added

---
+++ src/entity/artifact_entity.py
@@ -0,0 +1,8 @@
+# Output container
+from dataclasses import dataclass
+
+
+@dataclass
+class DataIngestionArtifact:
+    trained_file_path:str 
+    test_file_path:str
---
+++ src/entity/config_entity.py
@@ -0,0 +1,29 @@
+# Where should things be saved?
+import os
+from src.constants import *
+from dataclasses import dataclass
+from datetime import datetime
+
+TIMESTAMP: str = datetime.now().strftime("%m_%d_%Y_%H_%M_%S")
+
+@dataclass
+class TrainingPipelineConfig:
+    pipeline_name: str = PIPELINE_NAME
+    artifact_dir: str = os.path.join(ARTIFACT_DIR, TIMESTAMP) #artifact_dir = os.path.join("artifact", TIMESTAMP)
+    timestamp: str = TIMESTAMP
+
+
+training_pipeline_config: TrainingPipelineConfig = TrainingPipelineConfig()
+
+@dataclass
+class DataIngestionConfig:
+    data_ingestion_dir: str = os.path.join(training_pipeline_config.artifact_dir, DATA_INGESTION_DIR_NAME) 
+    feature_store_file_path: str = os.path.join(data_ingestion_dir, DATA_INGESTION_FEATURE_STORE_DIR, FILE_NAME) 
+    #feature_store_file_path =artifact/timestamp/data_ingestion/feature_store/data.csv
+
+    training_file_path: str = os.path.join(data_ingestion_dir, DATA_INGESTION_INGESTED_DIR, TRAIN_FILE_NAME)
+    #training_file_path =artifact/timestamp/data_ingestion/ingested/train.csv
+
+    testing_file_path: str = os.path.join(data_ingestion_dir, DATA_INGESTION_INGESTED_DIR, TEST_FILE_NAME)
+    train_test_split_ratio: float = DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO
+    collection_name:str = DATA_INGESTION_COLLECTION_NAME
